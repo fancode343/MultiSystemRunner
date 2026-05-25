4f0d4910ad592f51b575359127c33ae54d6e676e
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-05-25 23:04:45 +0800

    Implement file listing in MSR class
    Added functionality to list files in a specified directory.

--- src/MSR.java
+++ src/MSR.java
@@ -1,8 +1,34 @@
+import java.io.File;
+import java.util.*;
+
 public class MSR {
     public static void main(String[] args) {
+        String folder = "Systems";
+        String[] files = getSystems.listFiles(folder);
+        System.out.println(Arrays.toString(files));
         
     }
-    public static void print(Object o){
-        System.out.println(o);
+    public static class getSystems {
+        public static String[] listFiles(String path) {
+            File dir = new File(path);
+            File[] listOfFiles = dir.listFiles();
+            if (listOfFiles == null) {
+                return new String[0];
+            }
+            List<String> result = new ArrayList<>();
+            for (File file : listOfFiles) {
+                if (file.isFile()) {
+                    if(path.equals("Systems")) {
+                        result.add(file.getName());
+                    }else{
+                        result.add(path.substring(8)+"/"+file.getName());
+                    }
+                } else if (file.isDirectory()) {
+                    String[] subFiles = listFiles(path + "/" + file.getName());
+                    result.addAll(Arrays.asList(subFiles));
+                }
+            }
+            return result.toArray(new String[0]);
+        }
     }
 }
